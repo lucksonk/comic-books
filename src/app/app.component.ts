ca0acd13fdd7151545674a59e93948d3ed869eba
Luckson Karikoga <luckson.karikoga@gmail.com>
2018-08-15 22:32:32 +0200

    added images and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,33 @@ import { ComicApiService } from './services/comic-api.service';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  data;
+
+  heroes = [];
+
   constructor(private api: ComicApiService) {}
 
   ngOnInit() {
     console.log('Init ...');
-    this.api.allHeroes().subscribe(res => {console.log(res); if (res) { this.data = res; } });
+    this.api.allHeroes().subscribe(
+      res => {
+        console.log(res);
+        let temp: any = res;
+
+         if (temp) {
+            temp.data.results.forEach(element => {
+              if (element.description) {
+                   this.heroes.push({
+                       id: element.id,
+                       name: element.name,
+                       description: element.description,
+                       modified: element.modified,
+                       image: element.thumbnail.path + '.' + element.thumbnail.extension,
+                       details_url: element.urls[0].url
+                    });
+                  }
+            });
+         }
+
+      });
   }
 }
